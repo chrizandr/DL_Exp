Fix scaling factors

transformers/alibi.py
@@ -48,7 +48,7 @@ class ALiBiBlock(nn.Module):
         bias = torch.tensor(
             [[-1*(j-i) if i <= j else 0for i in range(key_shape)]
             for j in range(queries_shape)])
-        y = (torch.bmm(Q_, K_) + bias) / np.sqrt(keys.shape[1])
+        y = (torch.bmm(Q_, K_) + bias) / np.sqrt(K_.shape[1])
 
         if masking:
             mask = torch.tensor([[0 if i <= j else -1 * float("inf")

transformers/transformer.py
@@ -37,7 +37,8 @@ class SDPAttentionBlock(nn.Module):
         K_ = self.WK(keys)
         K_ = K_.permute(0, 2, 1)
 
-        y = torch.bmm(Q_, K_) / np.sqrt(keys.shape[1])
+
+        y = torch.bmm(Q_, K_) / np.sqrt(K_.shape[1])
         if masking:
             key_shape = keys.shape[1]
             queries_shape = queries.shape[1]

transformers/vision_transformer.py
@@ -71,7 +71,7 @@ class SDPAttentionBlock(nn.Module):
         K_ = self.WK(keys)
         K_ = K_.permute(0, 2, 1)
 
-        y = torch.bmm(Q_, K_) / np.sqrt(keys.shape[1])
+        y = torch.bmm(Q_, K_) / np.sqrt(K_.shape[1])
         if masking:
             num_tokens = keys.shape[1]
             mask = torch.tensor([[0 if i <= j else -1 * float("inf")